373ee21364229870bd50fef595e68fcd44521b19
Eugene <eugene.bryant@mymail.champlain.edu>
2022-09-26 14:11:36 -0400

    started working on Lab 2

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -1,1 +1,16 @@
 #include "Agent.h"
+#include "Cat.h"
+#include "Catcher.h"
+#include "World.h"
+
+int main() 
+{
+	World* world;
+	Cat* cat;
+	Catcher* catcher;
+
+	cat->Move(world);
+    catcher->Move(world);
+
+	return 0;
+}
