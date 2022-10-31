19555e27f6eeea703b331ee05305f55e614fb839
Eugene <eugene.bryant@mymail.champlain.edu>
2022-10-31 13:07:59 -0400

    adding cmake

--- examples/maze/MazeGenerator.cpp
+++ examples/maze/MazeGenerator.cpp
@@ -2,51 +2,8 @@
 #include "World.h"
 
 bool MazeGenerator::Step(World* world) {
-  //Recursive Backtracking
-	
-	Point2D grid[100];
+  //Eller's Algorithm
 
-	Point2D point;
-
-
-	for (int i = 0; i < world->GetSize(); i++) 
-	{
-		
-	}
-
-	if (world->GetNorth(point)) 
-	{
-          world->SetNorth(point, false);
-          world->SetEast(point, true);
-          world->SetSouth(point, true);
-          world->SetWest(point, true);
-    }
-    
-	if (world->GetEast(point)) 
-	{
-      world->SetEast(point, false);
-      world->SetSouth(point, true);
-      world->SetWest(point, true);
-      world->SetNorth(point, true);
-    }
-    
-	if (world->GetSouth(point)) 
-	{
-      world->SetSouth(point, false);
-      world->SetWest(point, true);
-      world->SetNorth(point, true);
-      world->SetSouth(point, true);
-    }
-    
-	if (world->GetWest(point)) 
-	{
-      world->SetWest(point, false);
-      world->SetNorth(point, true);
-      world->SetEast(point, true);
-      world->SetSouth(point, true);
-    }
-	
-
-  return true;
+	return true;
 }
 void MazeGenerator::Clear(World* world) {}
