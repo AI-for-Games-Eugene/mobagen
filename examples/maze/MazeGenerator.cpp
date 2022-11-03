c793c3945d75cae6095d1e82ff5356a57a6d4959
Eugene <eugene.bryant@mymail.champlain.edu>
2022-11-03 14:14:40 -0400

    algorithm is still a work-in-progress

--- examples/maze/MazeGenerator.cpp
+++ examples/maze/MazeGenerator.cpp
@@ -25,10 +25,10 @@ bool MazeGenerator::Step(World* world) {
   //std::map<int, std::map<int, bool>> visited;
   //Random random;
 
+        /*****************attempt**************************/
   int end = world->GetSize();
   int start = -world->GetSize() + world->GetSize() + 1;
 
-	//x, y = rand(width), rand(height)
   int x = random.Range(start, end);
   int y = random.Range(start, end);
   auto walk = stack.back();
@@ -39,54 +39,56 @@ bool MazeGenerator::Step(World* world) {
     visited[walk.y][walk.x];
   
   } while (!stack.empty());
-	//x, y = walk (grid, x, y)
-	//x, y = hunt (grid) unless x
-	//break unless x
-	//end
 
-	//def walk (grid, x, y)
-	//[N, S, E, W].shuffle.each do |dir|
-	// #...
-	//end
-	//nil
-	//end
-
-	//nx, ny = x + DX [dir], y + DY[dir]
-	//if nx >= 0 && ny >= 0 && ny < grid.length && nx < grid[ny].length && grid[ny][nx] == 0
   if (visited[walk.y][walk.x]) {
   
   }
-	//#...
-	//end
+	return visited[walk.y][walk.x];
 
-	//grid[y][x] | = dir
-	//grid[ny][nx] | = OPPOSITE[dir]
-	//return [nx, ny]
   return visited[walk.y][walk.x];
+        /************************************************************/
 
-	//def hunt(grid)
-	//grid.each_with_index do |row, y|
-	//row.each_with_index do |cell, x|
-	//next unless cell == 0
-	//#...
-	//end
-	//end
-	//nil
-	//end
+  //def walk(grid, x, y)
+  //[ N, S, E, W ].shuffle.each do | dir |
+  //nx, ny = x + DX[dir], y + DY[dir] 
+  //if nx >= 0 && ny >= 0 && ny < grid.length && nx < grid[ny].length && grid[ny][nx] == 0
+  //grid[y][x] |= dir
+  //grid[ny][nx] |= OPPOSITE[dir]
+  //return [ nx, ny ]
+  //end
+  //end
+  //nil
+  //end
+  //def hunt(grid)
+  //grid.each_with_index do |row, y|
+  //display_maze(grid, y)
+  //sleep 0.02
+  //row.each_with_index do |cell, x|
+  //next unless cell == 0
+  //neighbors = []
+  //neighbors << N if y > 0 && grid[y - 1][x] != 0
+  //neighbors << W if x > 0 && grid[y][x - 1] != 0
+  //neighbors << E if x + 1 < grid[y].length && grid[y][x + 1] != 0
+  //neighbors << S if y + 1 < grid.length && grid[y + 1][x] != 0
+  //direction = neighbors[rand(neighbors.length)] or next
+  //nx, ny = x + DX[direction], y + DY[direction]
+  //grid[y][x] |= direction
+  //grid[ny][nx] |= OPPOSITE[direction]
+  //return [ x, y ]
+  //end
+  //end
+  //nil
+  //end
+  //print "\e[2J" #clear the screen
+  //x, y = rand(width), rand(height)
+  //loop do
+  //display_maze(grid)
+  //sleep 0.02
+  //x, y = walk(grid, x, y)
+  //x, y = hunt(grid) unless x
+  //break unless x
+  //end
 
-	//neighbors = []
-	//neighbors << N if y > 0 && grid[y-1][x] != 0
-	//neighbors << W if x > 0 && grid[y][x-1] != 0
-	//neighbors << E if x+1 < grid[y].length && grid[y][x+1] != 0
-	//neighbors << S if y+1 < grid.length && frid[y+1][x] != 0
-
-	//direction = neighbors[rand(neighbors.length)] or next
-
-	//nx, ny = x + DX[direction], y + DY[direction]
-	//grid[y][x] |= direction
-	//grid[ny][nx] |= OPPOSITE[direction]
-	//return[x, y]
-  return visited[walk.y][walk.x];
 
 	return true;
 }
